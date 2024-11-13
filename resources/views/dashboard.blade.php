492bd208008cfd60d08c0db68c3e3ce2ea860cdf
Jhosue Pastor <pastor.jhosue07@gmail.com>
2024-11-13 00:46:09 -0500

    Primer commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,15 @@
+<!-- resources/views/dashboard.blade.php -->
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+
+
+    <!-- Barra de navegación -->
+    <x-barra-nav />
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+                <h3>Bienvenido al Dashboard</h3>
+                <!-- Aquí puede ir más contenido del dashboard -->
             </div>
         </div>
     </div>
